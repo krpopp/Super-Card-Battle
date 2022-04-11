0688eadcaaa3a0482d682b66d3541a5ba24ce408
Karina <karina.popp@nyu.edu>
2022-04-11 09:46:01 -0400

    force

---
+++ CardBattle/Assets/Scripts/Card.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : CardBehavior
+{
+
+    public AudioClip flipSound;
+    SpriteRenderer mySpriteRend;
+
+    protected override void OnMouseDown()
+    {
+        base.OnMouseDown();
+        GetComponent<Animator>().SetTrigger("doBounce");
+    }
+
+    protected override void SelectedChanges()
+    {
+        base.SelectedChanges();
+        GetComponent<AudioSource>().PlayOneShot(flipSound);
+    }
+}
